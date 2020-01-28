0bf5e2870301acabf8405ed31a039dbe60bc99ac
Chris <patte539@umn.edu>
2020-01-27 21:05:01 -0600

    Add businesses API

---
+++ create.js
@@ -0,0 +1,17 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    Item: data
+  };
+  console.log(params);
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
