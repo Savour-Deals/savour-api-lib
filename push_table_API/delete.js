697511a6259859dd05893c75952909895d432e5c
Chris <patte539@umn.edu>
2020-02-02 12:14:30 -0600

    Add push table API

---
+++ push_table_API/delete.js
@@ -0,0 +1,20 @@
+import * as dynamoDbLib from "../libs/dynamodb-lib";
+import { success, failure } from "../libs/response-lib";
+
+export async function main(event, context) {
+  const params = {
+    TableName: process.env.pushTbl,
+		// 'Key' defines the partition key and sort key of the item to be retrieved
+    // - 'unique_id': identifying one message
+    Key: {
+      unique_id: event.pathParameters.uid,
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("delete", params);
+    return success({ status: true });
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
