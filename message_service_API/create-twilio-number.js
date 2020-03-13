321cafec88bf9efcc1891bc3b1b4f7de5dcf45fb
Chris <patte539@umn.edu>
2020-03-12 20:38:29 -0500

    Add twilio calls for number provisioning

--- message_service_API/create-twilio-number.js
+++ message_service_API/create-twilio-number.js
@@ -1,11 +1,41 @@
 import * as dynamoDbLib from "../libs/dynamodb-lib";
 import { success, failure } from "../libs/response-lib";
+import * as dynamoDbLib from "../libs/dynamodb-lib";
+const client = require('twilio')(process.env.accountSid, process.env.authToken);
 
 export async function main(event, context) {
 	const data = JSON.parse(event.body);
 
-	var twilioNumber = "";
+	//query for phone number in US
+	var twilioNumberResource = await client.availablePhoneNumbers('US')
+		.local
+		.list({
+			// nearLatLong: '37.840699, -122.461853',
+			// distance: 50,
+			areaCode: '612',
+			excludeAllAddressRequired: true,
+			// inRegion: 'CA',
+			limit: 1
+		})
+		.then(local => {
+			return local[0];
+		}).catch((err) => {
+			//error occured, return error to caller
+			return failure({ status: false, error: err });
+		});
+	
+	//provision phone number
+	let twilioNumber = await client.incomingPhoneNumbers
+		.create({phoneNumber: twilioNumberResource.phoneNumber})
+		.then(incoming_phone_number => {
+			return incoming_phone_number.phoneNumber;
+		})
+		.catch((err) => {
+			//error occured, return error to caller
+			return failure({ status: false, error: err });
+		});
 
+	//store new number in DB
 	const params = {
 		TableName: process.env.businessesTbl,
 		// 'Key' defines the partition key and sort key of the item to be retrieved
