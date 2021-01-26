757d781629e00d0bb05b3b9af8a63eda475dcdec
Chris <patte539@umn.edu>
2021-01-25 20:24:09 -0600

    Add redirect function

--- src/url_shortener/redirect.js
+++ src/url_shortener/redirect.js
@@ -1,3 +1,27 @@
-export default async function main(longUrl, shortUrlDomain) {
+import * as dynamoDbLib from "../../common/dynamodb-lib";
 
+export default async function main(event, context) {
+	const params = {
+    TableName: process.env.redirectTable,
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    // - 'unique_id': id identifying short -> long url mapping
+    Key: {
+      unique_id: event.token,
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("get", params);
+    if (result.Item) {
+      const url = response.Item.destination_url;
+      const content = "<html><body>Moved: <a href=\"" + url + "\">" + url + "</a></body></html>"
+      return {"destination_url": url, "content": content}
+    } else {
+      content = "<html><body><h1>404: Not Found</h1></body></html>"
+      return {"content": content}
+    }
+  } catch (e) {
+    content = "<html><body><h1>404: Not Found!</h1></body></html>"
+    return {"content": content}
+  }
 }
